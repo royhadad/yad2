224aee5db2e2e53f80f86f8cc016168e411a24df
royhadad <royhadad98@gmail.com>
2020-03-19 16:13:38 +0200

    stable release version, still need to fix csp - script-src policy to something safe

--- yad2-back/index.js
+++ yad2-back/index.js
@@ -5,6 +5,7 @@ const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
 const favicon = require('serve-favicon');
+const setHeaders = require('./middleWare/setHeaders');
 require('dotenv').config();
 
 //choose port
@@ -34,15 +35,7 @@ const serveReactApp = () => {
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://maps.googleapis.com https://maps.gstatic.com; img-src 'self' https://royhadad-yad2.s3-eu-west-1.amazonaws.com https://twitter.com https://pbs.twimg.com; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; frame-ancestors 'none';");
-    res.setHeader("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin");
-    res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains");
-    res.setHeader("X-Content-Type-Options", "nosniff");
-    res.setHeader("X-Frame-Options", "DENY");
-    res.setHeader("X-XSS-Protection", "1; mode=block");
-    return next();
-});
+app.use(setHeaders);
 
 if (isDev) {
     app.use(morgan('dev'));
---
+++ yad2-back/middleWare/setHeaders.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://maps.googleapis.com https://maps.gstatic.com; img-src 'self' https://royhadad-yad2.s3-eu-west-1.amazonaws.com https://twitter.com https://pbs.twimg.com; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com; frame-ancestors 'none';");
+    res.setHeader("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin");
+    res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains");
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+    next();
+};
