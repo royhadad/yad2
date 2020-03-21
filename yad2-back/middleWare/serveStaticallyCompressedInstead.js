603c8ab336b406591641ae2ee6c4ba8d9e7a1ce8
royhadad <royhadad98@gmail.com>
2020-03-21 14:44:16 +0200

    testing

--- yad2-back/middleWare/serveStaticallyCompressedInstead.js
+++ yad2-back/middleWare/serveStaticallyCompressedInstead.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const PATH_TO_BUILD = path.join(__dirname, '../../yad2-front', 'build');
 const fs = require('fs');
-
+//test
 module.exports = (req, res, next) => {
     if (/\.js$/.test(req.originalUrl) && req.headers["accept-encoding"].includes("gzip")) {
         const gzippedPath = path.join(PATH_TO_BUILD, req.originalUrl + '.gz');
