0525d0b32a95e001379d3abfc054b8d08e6511bf
royhadad <royhadad98@gmail.com>
2020-03-21 17:22:51 +0200

    stable release

--- yad2-back/routes/utils.js
+++ yad2-back/routes/utils.js
@@ -3,7 +3,6 @@ const secretVariables = require('dotenv').config().parsed;
 const ERROR_CODES = require('../errorCodes.js');
 const ResponseObj = require('../ResponseObj.js');
 const express = require('express');
-const request = require('request');
 const router = express.Router();
 const routePrefix = '/utils';
 //#endregion
