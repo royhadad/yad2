a24710f942dfcfbdf150b7b5b0bf7150fe1d5262
royhadad <royhadad98@gmail.com>
2020-06-18 16:05:32 +0300

    modifying content security policy to allow file uploads to s3 in production

--- yad2-back/src/middleware/setHeaders.js
+++ yad2-back/src/middleware/setHeaders.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src https://s3.eu-west-1.amazonaws.com/royhadad-yad2; script-src 'self' 'sha256-m5KNzjvGgQ8MgzI8EMIRHyNyascjtXxEnY9EhqPShvM=' https://*.googleapis.com; img-src https: data:; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; frame-ancestors 'none';");
+    res.setHeader("Content-Security-Policy", "default-src 'self'; connect-src *; script-src 'self' 'sha256-m5KNzjvGgQ8MgzI8EMIRHyNyascjtXxEnY9EhqPShvM=' https://*.googleapis.com; img-src https: data:; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; frame-ancestors 'none';");
     res.setHeader("Referrer-Policy", "no-referrer, strict-origin-when-cross-origin");
     res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains");
     res.setHeader("X-Content-Type-Options", "nosniff");
